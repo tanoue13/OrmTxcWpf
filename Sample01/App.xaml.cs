216ef4ed02c68041f6767e57f4fad2b664797d4c
田上 雅明 <tanoue13@nok.co.jp>
2021-06-06 13:47:40 +0900

    ちょこっと修正：AppのStartupイベントハンドラを作成。

--- Sample01/App.xaml.cs
+++ Sample01/App.xaml.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Sample01 {
+namespace Sample01
+{
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application {
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var window = new MainWindow();
+            window.Show();
+        }
     }
+
 }
