d5f077e15db19895266f2fa7d43155674d7720e6
田上 雅明 <tanoue13@nok.co.jp>
2021-06-29 23:46:04 +0900

    作業の一時保存：とりあえず、多重起動を防止できた。

--- Sample01/App.xaml.cs
+++ Sample01/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
 
 namespace Sample01
@@ -10,10 +11,32 @@ namespace Sample01
     public partial class App : Application
     {
 
+        /// <summary>
+        /// セマフォ（多重起動防止のために使用する）
+        /// </summary>
+        private static Semaphore Semaphore = null;
+
+        private static readonly string ApplicationName = "Sample01";
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            var window = new MainWindow();
-            window.Show();
+            // 多重起動を行わないため、セマフォを作成する。
+            App.Semaphore = new Semaphore(1, 1, ApplicationName, out bool createdNew);
+            //
+            if (!createdNew)
+            {
+                string message = String.Format("既に起動されています。[アプリケーション名：{0}]", ApplicationName);
+                //var exception = new ApplicationException(message);
+                //throw exception;
+                MessageBox.Show(message);
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                // Windowを表示する。
+                var window = new MainWindow();
+                window.Show();
+            }
         }
 
     }
