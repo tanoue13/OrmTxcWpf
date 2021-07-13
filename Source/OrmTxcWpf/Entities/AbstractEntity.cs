adcdcd93720ba821ccea692fdddbff9b646c0dda
田上 雅明 <tanoue13@nok.co.jp>
2021-07-13 15:07:44 +0900

    作業の一時保存：AbstractEntityを作成。

---
+++ Source/OrmTxcWpf/Entities/AbstractEntity.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using OrmTxcWpf.Attributes;
+using OrmTxcWpf.DataModels;
+using OrmTxcWpf.Utils;
+
+namespace OrmTxcWpf.Entities
+{
+
+    /// <summary>
+    /// Entityの基底クラス。
+    /// </summary>
+    public abstract class AbstractEntity : BindableDataModel
+    {
+
+        /// <summary>
+        /// <paramref name="dataRow"/>から値を取得し、プロパティを設定する。
+        /// </summary>
+        /// <param name="dataRow"></param>
+        public virtual void SetProperties(DataRow dataRow)
+        {
+            // DataTable.Columnsを取得する。
+            DataColumnCollection dataColumnCollection = dataRow.Table.Columns;
+            // DataTable.Columnsに存在するカラム属性のみを取得する。
+            IEnumerable<PropertyInfo> properties = this.GetColumnAttributes()
+                .Where(prop => dataColumnCollection.Contains(prop.GetCustomAttribute<ColumnAttribute>(false).ColumnName));
+            //
+            // プロパティを設定する。
+            foreach (PropertyInfo property in properties)
+            {
+                this.SetProperty(dataRow, property);
+            }
+        }
+        protected virtual void SetProperty(DataRow dataRow, PropertyInfo propertyInfo)
+        {
+            string columnName = propertyInfo.GetCustomAttribute<ColumnAttribute>(false).ColumnName;
+            if (dataRow.IsNull(columnName))
+            {
+                // DBNullの場合、nullを設定する。
+                propertyInfo.SetValue(this, null);
+            }
+            else
+            {
+                object value = dataRow[columnName];
+                propertyInfo.SetValue(this, value);
+            }
+        }
+
+    }
+
+}
---
+++ Source/OrmTxcWpf/Utils/EntityUtils.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using OrmTxcWpf.Attributes;
+using OrmTxcWpf.Entities;
+
+namespace OrmTxcWpf.Utils
+{
+
+    /// <summary>
+    /// Entityに関するユーティリティクラス。（AbstractEntityの拡張メソッドを含む）
+    /// </summary>
+    public static class EntityUtils
+    {
+
+        /// <summary>
+        /// テーブル名を取得する。
+        /// </summary>
+        /// <param name="abstractEntity"></param>
+        /// <returns></returns>
+        public static string GetTableName(this AbstractEntity abstractEntity)
+        {
+            Type type = abstractEntity.GetType();
+            return EntityUtils.GetTableName(type);
+        }
+        /// <summary>
+        /// テーブル名を取得する。
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
+        public static string GetTableName<TEntity>() where TEntity : AbstractEntity
+        {
+            Type type = typeof(TEntity);
+            return EntityUtils.GetTableName(type);
+        }
+        /// <summary>
+        /// テーブル名を取得する。
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static string GetTableName(Type type)
+        {
+            // テーブル名を取得する。
+            string tableName = type.GetCustomAttribute<TableAttribute>(false).TableName;
+            // 結果を戻す。
+            return tableName;
+        }
+
+        /// <summary>
+        /// カラム属性を取得する。
+        /// </summary>
+        /// <param name="abstractEntity"></param>
+        /// <returns></returns>
+        public static IEnumerable<PropertyInfo> GetColumnAttributes(this AbstractEntity abstractEntity)
+        {
+            Type type = abstractEntity.GetType();
+            return EntityUtils.GetColumnAttributes(type);
+        }
+        /// <summary>
+        /// カラム属性を取得する。
+        /// </summary>
+        /// <param name="abstractEntity"></param>
+        /// <returns></returns>
+        public static IEnumerable<PropertyInfo> GetColumnAttributes<E>() where E : AbstractEntity
+        {
+            Type type = typeof(E);
+            return EntityUtils.GetColumnAttributes(type);
+        }
+        /// <summary>
+        /// カラム名を取得する。
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static IEnumerable<PropertyInfo> GetColumnAttributes(Type type)
+        {
+            // カラム属性を取得する。
+            IEnumerable<PropertyInfo> attributes = type
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                .Where(prop => prop.GetCustomAttribute<ColumnAttribute>(false) != null);
+            // 結果を戻す。
+            return attributes;
+        }
+
+        /// <summary>
+        /// <paramref name="dataRow"/>の値を使用してEntityを生成する。
+        /// </summary>
+        /// <typeparam name="TEntity">生成するEntityの型</typeparam>
+        /// <param name="dataRow">Entityに設定する値</param>
+        /// <returns></returns>
+        public static TEntity Create<TEntity>(DataRow dataRow) where TEntity : AbstractEntity, new()
+        {
+            // インスタンスを生成する。
+            TEntity entity = new TEntity();
+            // dataRowから値を取得し、エンティティに設定する。
+            entity.SetProperties(dataRow);
+            // 結果を戻す。
+            return entity;
+        }
+
+    }
+
+}
