008c847c0accf2193a6263a9aded8cbc2c3a3841
田上 雅明 <tanoue13@nok.co.jp>
2021-07-13 14:51:19 +0900

    作業の一時保存：Attributeクラスを作成。

---
+++ Source/OrmTxcWpf/Attributes/PrimaryKeyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OrmTxcWpf.Attributes {
+
+    /// <summary>
+    /// 主キー属性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class PrimaryKeyAttribute : Attribute {
+
+        public PrimaryKeyAttribute() {
+        }
+
+    }
+
+}
